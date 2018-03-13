69ff988a41062a53380723e643f83ea532e91d90
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-12 23:01:44 -0400

    working version

--- 1-UI/textAreaOverlay/_/script.js
+++ 1-UI/textAreaOverlay/_/script.js
@@ -1,10 +1,24 @@
 const textarea = document.getElementById("textInput")
 const h1 = document.querySelector(".main_text_container h1")
 
+window.onload = () => {
+  textarea.focus()
+}
+
 textarea.addEventListener("keyup", () => {
   if(textarea.value) {
-    h1.style.display = "none"
+    h1.style.opacity = 0
   } else {
-    h1.style.display = "inline-block"
+    h1.style.opacity = 100
   }
+})
+
+textarea.addEventListener("focusin", () => {
+  if(textarea.value) {
+    h1.style.opacity = 0
+  }
+})
+
+textarea.addEventListener("focusout", () => {
+  h1.style.opacity = 100
 })
